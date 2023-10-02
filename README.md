8ebbda2e92bcc11373d2693b1cda6e645d3fcc77
ffuuugor <shilov@nigma.ru>
2023-10-02 22:53:24 +0100

    init

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # myfr24-converter
-A tool to convert "App in the Air" data export to MyFlightradar24 format
+This is a command-line tool to convert "App in the Air" data export to a csv format accepted by MyFlightradar24. 
+It's built mainly for one-time migration of your flight history from "App in the Air" to MyFlightradar24.
+
+## How to use
+
+### Step 1: Export "App in the Air" data
+
+In the "App in the Air" app: **Profile | Settings | Export account**.
+
+You will then receinve *an email* containing your full data export in a file named `data.txt`. 
+This is the file you will then need to pass to the converter script as an input.
+
+### Step 2: Run the converter script
+
+Run the script from within the top-level project folder 
+(or provide a qualified path to the `data` folder in a `--data-dir` argument)
+
+```shell
+python converter.py --input <PATH_TO_DATA_TXT> --output <PATH_TO_OUTPUT_CSV>
+```
+
+### Step 3: Upload the result to MyFlightradar24
+
+Upload the converter's output to https://my.flightradar24.com/settings/import
+
+## Limitations and quirks
+
+Despite claiming to accept Openflights.org export format, MyFlightradar24 doesn't always correctly recognize 
+the airline and the aircraft model from the openflights export. 
+
+Surprisingly, MyFlightradar24 doesn't accept IATA/ICAO codes for airlines/planes, but rather relies on internal ids 
+or full names. 
+On top of that, their internal ids don't match with the openflights database, and are not easily accessible.
+
+Full names in the openflights db *mostly* match MyFR24's expected names, but not always.
+For the relatively rare occasion of the mismatch, manually created `data/extra_airlines.dat` and `data/extra_planes.dat` 
+provide overrides or additions to the openflights db 
+(openflights database is no longer regularly updated, so new airlines are missing).
+These files are by no means exhaustive, and your export will probably contain a few mismatched airlines or planes. If
+you notice a large amount of missing records in your MyFR24 flight import please consider adding to the override files.
+(The expected names for airlines/planes on MyFR24 can be found in the suggestion box when manually adding a flight).
+
+Some known issues:
+
+* Boeing 737 MAX cannot be added to MyFR24. I was unable to find the right spelling that would be recognized by the import script
+* Wrong airline name can be picked in case of an IATA code conflict. 2-letter IATA codes are not unique worldwide, however
+"App in the Air" uses them as the only airline identifier in the data export. The coverter doesn't check whether a flight
+actually exists and would pick the *last* matching airline in the openflights database. 
+* The script ignores `Ownership.MINE` and `Ownership.NOT_MINE` flags and adds all the flights to the output regardless
+
+## Acknowledgments
+
+As a first step in its execution, the script will download airlines 
+and planes databases provided by (Openflights)[https://openflights.org/data.html].
+
+We are very thankful to OpenFlights for maintaining and publishing the data.
+
---
+++ converter.py
@@ -0,0 +1,187 @@
+import argparse
+import logging
+import os
+import urllib.request
+from typing import Tuple
+
+import pandas as pd
+
+OPENFLIGHTS_PLANES_URL = "https://raw.githubusercontent.com/jpatokal/openflights/master/data/planes.dat"
+OPENFLIGHTS_AIRLINES_URL = "https://raw.githubusercontent.com/jpatokal/openflights/master/data/airlines.dat"
+
+APP_IN_THE_AIR_FIELDS = [
+    "class",
+    "seat",
+    "pnr",
+    "etkt",
+    "what",
+    "fare_class",
+    "import_source",
+    "airline_iata",
+    "flight_number",
+    "plane_iata",
+    "from_iata",
+    "to_iata",
+    "dep_utc",
+    "arr_utc",
+    "dep_local",
+    "arr_local",
+    "booked",
+]
+
+OPENFLIGHTS_AIRLINES_FIELDS = [
+    "full_name",
+    "alias",
+    "iata",
+    "icao",
+    "callsign",
+    "country",
+    "is_active",
+]
+OPENFLIGHTS_PLANE_FIELDS = ["full_name", "iata", "dafif"]
+
+OPENFLIGHTS_AIRLINES_DATA_FILE = "airlines.dat"
+OPENFLIGHTS_PLANES_DATA_FILE = "planes.dat"
+EXTRA_PLANES_DATA_FILE = "extra_planes.dat"
+EXTRA_AIRLINES_DATA_FILE = "extra_airlines.dat"
+
+
+def format_timedelta(td: pd.Timedelta):
+    hours = 24 * td.days + (td.seconds // (60 * 60))
+    minutes = (td.seconds // 60) % 60
+
+    return f"{hours:02}:{minutes:02}"
+
+
+def download_openflights(data_dir: str) -> None:
+    if not os.path.isdir(data_dir):
+        raise ValueError(f"Data dir {data_dir} doesn't exist. Please create the directory first.")
+
+    urllib.request.urlretrieve(OPENFLIGHTS_PLANES_URL, os.path.join(data_dir, OPENFLIGHTS_PLANES_DATA_FILE))
+    urllib.request.urlretrieve(OPENFLIGHTS_AIRLINES_URL, os.path.join(data_dir, OPENFLIGHTS_AIRLINES_DATA_FILE))
+
+
+def read_assets(data_dir: str) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    openflights_airlines_path = os.path.join(data_dir, OPENFLIGHTS_AIRLINES_DATA_FILE)
+    openflights_planes_path = os.path.join(data_dir, OPENFLIGHTS_PLANES_DATA_FILE)
+    extra_airlines_path = os.path.join(data_dir, EXTRA_AIRLINES_DATA_FILE)
+    extra_planes_path = os.path.join(data_dir, EXTRA_PLANES_DATA_FILE)
+
+    airlines_df = pd.read_csv(openflights_airlines_path, index_col=0, header=None, names=OPENFLIGHTS_AIRLINES_FIELDS)
+    planes_df = pd.read_csv(openflights_planes_path, header=None, names=OPENFLIGHTS_PLANE_FIELDS)
+
+    airlines_df = airlines_df[airlines_df.is_active == "Y"]
+    if os.path.isfile(extra_airlines_path):
+        extra_airlines_df = pd.read_csv(extra_airlines_path, header=None, names=OPENFLIGHTS_AIRLINES_FIELDS)
+        airlines_df = pd.concat([airlines_df, extra_airlines_df]).drop_duplicates(subset="iata", keep="last")
+
+    if os.path.isfile(extra_planes_path):
+        extra_planes_df = pd.read_csv(extra_planes_path, header=None, names=OPENFLIGHTS_PLANE_FIELDS)
+        planes_df = pd.concat([planes_df, extra_planes_df]).drop_duplicates(subset="iata", keep="last")
+
+    return airlines_df, planes_df
+
+
+def read_flight_data(input: str) -> pd.DataFrame:
+    flights = []
+    is_flights_section = False
+
+    with open(input, "r") as f:
+        for line in f.readlines():
+            line = line.strip()
+            if len(line) == 0:
+                continue
+
+            if line.startswith("flights:"):
+                is_flights_section = True
+                continue
+
+            if line.startswith("hotels:"):
+                is_flights_section = False
+                continue
+
+            if not is_flights_section:
+                continue
+
+            arr = line.split(";")
+
+            if len(arr) != len(APP_IN_THE_AIR_FIELDS):
+                logging.warning(f"Unable to parse: {line}")
+                continue
+
+            flights.append(arr)
+
+    return pd.DataFrame(flights, columns=APP_IN_THE_AIR_FIELDS).replace("None", None)
+
+
+def convert(flights_df: pd.DataFrame, airlines_df: pd.DataFrame, planes_df: pd.DataFrame) -> pd.DataFrame:
+    def plane_iata_to_name(iata_code: str):
+        plane_info = planes_df[planes_df.iata == iata_code]
+        if len(plane_info) == 1:
+            return plane_info.iloc[0].full_name
+        else:
+            if iata_code != "None":
+                logging.debug(f"Found {len(plane_info)} records for {iata_code} plane designator. Skipping")
+            return None
+
+    def airline_iata_to_name(iata_code: str):
+        airline_info = airlines_df[(airlines_df.iata == iata_code) & (airlines_df.is_active == "Y")]
+        if len(airline_info) == 1:
+            return airline_info.iloc[0].full_name
+        else:
+            if iata_code != "None":
+                logging.debug(f"Found {len(airline_info)} records for {iata_code} airline designator. Skipping")
+            return None
+
+    df = pd.DataFrame()
+
+    df["Date"] = pd.to_datetime(flights_df.dep_local).dt.date
+    df["Flight number"] = flights_df.airline_iata + flights_df.flight_number.astype(int).astype(str)
+    df["From"] = flights_df.from_iata
+    df["To"] = flights_df.to_iata
+    df["Dep time"] = pd.to_datetime(flights_df.dep_local).dt.time
+    df["Arr time"] = pd.to_datetime(flights_df.arr_local).dt.time
+    df["Duration"] = (pd.to_datetime(flights_df.arr_utc) - pd.to_datetime(flights_df.dep_utc)).map(
+        lambda x: format_timedelta(x)
+    )
+    df["Airline"] = flights_df["airline_iata"].apply(airline_iata_to_name)
+    df["Aircraft"] = flights_df["plane_iata"].apply(plane_iata_to_name)
+    df["Seat number"] = flights_df.seat
+
+    return df
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Command-line tool to convert "App in the Air" data export to MyFlighradar24 format'
+    )
+
+    parser.add_argument(
+        "--data-dir",
+        type=str,
+        default="data",
+        help="Folder to store downloaded assets and extra data",
+    )
+    parser.add_argument(
+        "-s",
+        "--skip-download",
+        action="store_true",
+        help="Skips downloading external assets. Assets must already be present in the data folder",
+    )
+    parser.add_argument("-i", "--input", type=str, required=True, help="App in the Air export file path")
+    parser.add_argument("-o", "--output", type=str, required=True, help="Output csv path")
+
+    args = parser.parse_args()
+
+    if not args.skip_download:
+        download_openflights(args.data_dir)
+
+    airlines_df, planes_df = read_assets(args.data_dir)
+    flights_df = read_flight_data(args.input)
+
+    df = convert(flights_df, airlines_df, planes_df)
+    df.to_csv(args.output, index=False)
+
+
+if __name__ == "__main__":
+    main()
