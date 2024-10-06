4a4035bcae3bf60df745a9f77fb2c4f7d3c23ec4
uncletoxa <7903257+uncletoxa@users.noreply.github.com>
2024-10-07 00:02:18 +0200

    fix: download assets ont start

--- start.sh
+++ start.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 set -e
 
-echo "Current directory: $(pwd)"
-echo "Contents of /app:"
-ls -la /app
+echo "Downloading assets..."
+mkdir -p /app/data
+curl -o /app/data/planes.dat https://raw.githubusercontent.com/jpatokal/openflights/master/data/planes.dat
+curl -o /app/data/airlines.dat https://raw.githubusercontent.com/jpatokal/openflights/master/data/airlines.dat
 
-echo "Python version:"
-python --version
-
-echo "Pip list:"
-pip list
+echo "Verifying downloaded assets:"
+ls -la /app/data
 
 echo "Starting Nginx..."
 nginx
 
 echo "Starting Gunicorn..."
 GUNICORN_PATH=$(which gunicorn)
-echo "Gunicorn path: $GUNICORN_PATH"
 $GUNICORN_PATH --bind unix:/tmp/gunicorn.sock app:app
